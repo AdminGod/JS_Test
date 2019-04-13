9bcb7da63e6635e5aa9828e13b2c5b32b2351624
Abonent63 <mikhaylovVladymir@gmail>
2019-04-13 14:42:42 +0300

    less4(hw) - done

--- trash.js
+++ trash.js
@@ -1,3 +1,13 @@
 (function () {
+        const [a, b] = [1, 2];
 
+        const {prop1, ...other} = {
+            prop2: "222",
+            prop1: "111",
+            prop3: "333",
+            prop4: "444"
+        }
+        console.log("PROP1", prop1);
+        console.log(other);
+        console.log(other.prop2);
 })();
